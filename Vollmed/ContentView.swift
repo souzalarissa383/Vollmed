9d9c0c9fb1f30a998b83ceca083b0a2a9f5f588e
“larissa.ribeiro” <larissa.ribeiro@g.globo>
2024-07-03 18:45:28 -0300

    Initial Commit

---
+++ Vollmed/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Vollmed
+//
+//  Created by Larissa Souza on 03/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Vollmed/VollmedApp.swift
@@ -0,0 +1,17 @@
+//
+//  VollmedApp.swift
+//  Vollmed
+//
+//  Created by Larissa Souza on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct VollmedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
